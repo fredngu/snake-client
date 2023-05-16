404cadb0db7c9bf727d9f816cab77dcceb7a2014
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-05-15 20:26:39 -0400

    Update code to create connection object to allow user inputs to work in client

--- play.js
+++ play.js
@@ -1,8 +1,8 @@
 const net = require("net");
-const {connect} = require('./client');
+const { connect } = require('./client');
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+const connection = connect();
 
-setupInput();
+setupInput(connection);
